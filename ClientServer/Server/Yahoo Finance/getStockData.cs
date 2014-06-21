03adab15248b09cf618786850cfe201aa987c6bd
elmali <leevalve@gmail.com>
2014-06-21 08:43:48 -0700

    return type change

--- ClientServer/Server/Yahoo Finance/getStockData.cs
+++ ClientServer/Server/Yahoo Finance/getStockData.cs
@@ -11,19 +11,18 @@ namespace Server.Yahoo_Finance
     {
         private const string originURL = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20({0})&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
-        public static stockQuote getData(ObservableCollection<stockQuote> quotes)
+        public static void getData(ObservableCollection<stockQuote> quotes)
         {
             string query = String.Join("%2C", quotes.Select(s => "%22" + s.StockQuery + "%22").ToArray());
             string url = string.Format(originURL, query);
             XDocument resultXmlDoc = XDocument.Load(url);
-            stockQuote result = xmlProcess(quotes, resultXmlDoc);
-            return result;
+            xmlProcess(quotes, resultXmlDoc);
+           
         }
 
-        private static stockQuote xmlProcess(ObservableCollection<stockQuote> quotes, XDocument resultXmlDoc)
+        private static void xmlProcess(ObservableCollection<stockQuote> quotes, XDocument resultXmlDoc)
         {
             XElement results = resultXmlDoc.Root.Element("results");
-            stockQuote result = null;
 
             foreach (stockQuote quote in quotes)
             {
@@ -31,9 +30,9 @@ namespace Server.Yahoo_Finance
                 
                 quote.Name = q.Element("Name").Value;
                 quote.LastTradePrice = GetDecimal(q.Element("LastTradePriceOnly").Value);
-                result = quote;
+                
             }
-            return result;
+          
         }
 
         private static decimal? GetDecimal(string data)
