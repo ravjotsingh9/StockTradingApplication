af781bcd00948035ea4d2131a764fbfbd800f817
elmali <leevalve@gmail.com>
2014-06-23 16:15:02 -0700

    changes

--- ClientServer/Server/Users.cs
+++ ClientServer/Server/Users.cs
@@ -9,14 +9,18 @@ namespace Server
     {
         
         
-       /** //Dictionary<string, int> stockShares stores stock's name and shares
+        //Dictionary<string, int> stockShares stores stock's name and shares
         public class userInfo
         {
             public string cashBalance { get; set; }
             public Dictionary<string, int> stockShares { get; set; }
-        }**/
+            public userInfo()
+            {
+                stockShares = new Dictionary<string, int>();
+            }
+        }
 
-        /**private Dictionary<string, userInfo> m_userDictionary;
+        private Dictionary<string, userInfo> m_userDictionary;
         public Dictionary<string, userInfo> UserDictionary
         {
             get{
@@ -30,7 +34,7 @@ namespace Server
         public Users()
         {
             UserDictionary = new Dictionary<string, userInfo>();
-        }**/
+        }
 
 
         // file's name for users' information stores in the Disk
