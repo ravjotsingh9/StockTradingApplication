6fd2d1d9d23820d4295867357d306949770146a7
hangmiao <hahnmiao@gmail.com>
2014-06-24 16:18:25 -0700

    another testing

--- ClientServer/Client/Program.cs
+++ ClientServer/Client/Program.cs
@@ -8,7 +8,6 @@ namespace Client
     static class Program
     {
         //this is a testing statement to see whether i can commit or not- Amulya
-        // Hi I'm here. -for testing Claire
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
